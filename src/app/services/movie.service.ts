e6722d3042ba4f458d4edfa4a2c3ce2a0290f853
Andrei Gavriloiu <andreigavri@yahoo.com>
2023-10-23 20:43:11 +0300

    adding services (in order to work with the backend database)

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() {
+  }
+
+  createMovie(movie: any) {
+
+  }
+
+  updateMovie(movie: any) {
+
+  }
+
+  deleteMovie(movieId: string) {
+
+  }
+
+  readMovie() {
+
+  }
+}
